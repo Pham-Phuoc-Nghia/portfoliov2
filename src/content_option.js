baf94d9e56cfa3b24b7d7ca164e0ef32dea96ebf
Pham-Phuoc-Nghia <phamphuocnghia21082002@gmail.com>
2024-09-09 10:04:00 +0700

    add new mail

--- src/content_option.js
+++ src/content_option.js
@@ -1,147 +1,155 @@
 const logotext = "NGHIA.";
 const meta = {
-    title: "Nghia Portfolio",
-    description: "I’m Pham Phuoc Nghia data Font End developer,currently working in Berlin",
+  title: "Nghia Portfolio",
+  description:
+    "I’m Pham Phuoc Nghia data Font End developer,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m Pham Phuoc nghia",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites"
-    },
-    description: "I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️",
-    your_img_url: "https://i.imgur.com/m7wt3lC.jpg",
+  title: "I’m Pham Phuoc nghia",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+  },
+  description:
+    "I fell in love with programming and I have at least learnt something, I think… 🤷‍♂️",
+  your_img_url: "https://i.imgur.com/m7wt3lC.jpg",
 };
 
 const dataabout = {
-    title: "Know Who I'M",
-    aboutme: "Hi Everyone, I am Pham Phuoc Nghia from Go Vap district, Ho Chi Minh City.\n I am a final year Information Technology student enjoying the last days of my university studies.\n My passion lies in the field of Front-end, where I have the opportunity to turn creative ideas into visual reality on the web."};
-const worktimeline = [{
-        jobtitle: "Van lang university",
-        where: "information technology",
-        date: "2020-2024",
-    },
+  title: "Know Who I'M",
+  aboutme:
+    "Hi Everyone, I am Pham Phuoc Nghia from Go Vap district, Ho Chi Minh City.\n I am a final year Information Technology student enjoying the last days of my university studies.\n My passion lies in the field of Front-end, where I have the opportunity to turn creative ideas into visual reality on the web.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Van lang university",
+    where: "information technology",
+    date: "2020-2024",
+  },
 ];
 
-const skills = [{
-        name: "HTML",
-        value: 90,
-    },
-    {
-        name: "CSS",
-        value: 85,
-    },
-    {
-        name: "Javascript",
-        value: 80,
-    },
-    {
-        name: "React",
-        value: 60,
-    },
-    {
-        name: "GIT",
-        value: 85,
-    },
+const skills = [
+  {
+    name: "HTML",
+    value: 90,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 80,
+  },
+  {
+    name: "React",
+    value: 60,
+  },
+  {
+    name: "GIT",
+    value: 85,
+  },
 ];
 
-const services = [{
-        title: "Web developer",
-        description: "As a web developer, I leverage a combination of back-end and front-end knowledge to build high-quality websites.\n I use programming languages like HTML, CSS, JavaScript and popular frameworks like React to create user-friendly and interactive interfaces.",
-    },
-    
+const services = [
+  {
+    title: "Web developer",
+    description:
+      "As a web developer, I leverage a combination of back-end and front-end knowledge to build high-quality websites.\n I use programming languages like HTML, CSS, JavaScript and popular frameworks like React to create user-friendly and interactive interfaces.",
+  },
 ];
 
-const dataportfolio = [{
-        img: "https://i.imgur.com/1N28oQN.png",
-        description: "Online sales website",
-        link: "https://github.com/Pham-Phuoc-Nghia/shoppingonline",
-    },
-    // {
-    //     img: "https://picsum.photos/400/?grayscale",
-    //     description: "form_login",
-    //     link: "https://pham-phuoc-nghia.github.io/form__login/",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://imgur.com/a/We3jSaf.png",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "https://pham-phuoc-nghia.github.io/form__login/",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/300/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    {
-        img: "https://i.imgur.com/Njkia46.png",
-        description: "form_login",
-        link: "https://pham-phuoc-nghia.github.io/form__login/",
-    },
+const dataportfolio = [
+  {
+    img: "https://i.imgur.com/1N28oQN.png",
+    description: "Online sales website",
+    link: "https://github.com/Pham-Phuoc-Nghia/shoppingonline",
+  },
+  // {
+  //     img: "https://picsum.photos/400/?grayscale",
+  //     description: "form_login",
+  //     link: "https://pham-phuoc-nghia.github.io/form__login/",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://imgur.com/a/We3jSaf.png",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "https://pham-phuoc-nghia.github.io/form__login/",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/300/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  {
+    img: "https://i.imgur.com/Njkia46.png",
+    description: "form_login",
+    link: "https://pham-phuoc-nghia.github.io/form__login/",
+  },
 
-    // {
-    //     img: "https://picsum.photos/400/600/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/300/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/550/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/700/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
+  // {
+  //     img: "https://picsum.photos/400/600/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/300/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/550/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
+  // {
+  //     img: "https://picsum.photos/400/700/?grayscale",
+  //     description: "The wisdom of life consists in the elimination of non-essentials.",
+  //     link: "#",
+  // },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "phamphuocnghia21082002@gmail.com",
-    YOUR_FONE: "+84363866208",
-    description: "Feel free to connect with me",
-    // creat an emailjs.com account 
-    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_0x0j9dn",
-    YOUR_TEMPLATE_ID: "template_cyyus9l",
-    YOUR_USER_ID: "PlXpiGfqDGEOYN6vd",
+  YOUR_EMAIL: "phamphuocnghia21082002@gmail.com",
+  YOUR_FONE: "+84363866208",
+  description: "Feel free to connect with me",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_0x0j9dn",
+  YOUR_TEMPLATE_ID: "template_cyyus9l",
+  YOUR_USER_ID: "PlXpiGfqDGEOYN6vd",
 };
 
 const socialprofils = {
-    github: "https://github.com/Pham-Phuoc-Nghia",
-    facebook: "https://www.facebook.com/profile.php?id=100008471567537",
-    linkedin: "https://www.linkedin.com/in/pham-phuoc-nghia-031512274/",
-    twitter: "https://twitter.com",
+  github: "https://github.com/Pham-Phuoc-Nghia",
+  facebook: "https://www.facebook.com/profile.php?id=100008471567537",
+  linkedin: "https://www.linkedin.com/in/pham-phuoc-nghia-031512274/",
+  twitter: "https://twitter.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
